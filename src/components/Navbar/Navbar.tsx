d6cbfd1d76521ca4962f25e36b362f156abd57c4
juciley.costa <juciley.costa@e-unicred.com.br>
2024-10-14 19:46:21 -0300

    NextJs - ItemLink.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome" ;
 import { faUser, faHome, faGamepad, faRankingStar, faRoute } from "@fortawesome/free-solid-svg-icons";
 import { cn } from "@/helpers/cn";
+import Link from "next/link";
 
 type NavbarProps = React.ComponentProps<"nav">;
 type NavbarListProps = React.ComponentProps<"ul">;
 type NavbarListItemProps = React.ComponentProps<"li">;
+type NavbarListItemLinkProps = React.ComponentProps<typeof Link>;
 
 const NavBarList = ({ children, ...props }: NavbarListProps) => {
   return (
@@ -16,11 +18,21 @@ const NavBarList = ({ children, ...props }: NavbarListProps) => {
 
 const NavBarListItem = ({ children, ...props }: NavbarListItemProps) => {
   return (
-    <li className="my-2 mx-2 p-3 rounded-lg bg-transparent hover:bg-indigo-400/40 cursor-pointer" {...props}>
+    <li className="my-2 mx-2 p-3 rounded-lg hover:bg-indigo-400/40 cursor-pointer" {...props}>
       { children }
     </li>
   );
 };
+
+const NavbarListItemLink = ({ href, children, className, ...props }: NavbarListItemLinkProps) => {
+  return (
+    <NavBarListItem className={cn("p-0", className)}>
+      <Link href={href} className="my-2 p-3 flex items-center rounded-lg hover:bg-indigo-400/40" {...props}>
+        { children }
+      </Link>
+    </NavBarListItem>
+  );
+}
 
 export const Navbar = ({ className, ...props}: NavbarProps) => {
   return(
@@ -34,28 +46,28 @@ export const Navbar = ({ className, ...props}: NavbarProps) => {
 
         <nav className={cn("flex flex-col mt-8 w-full gap-5 border-t-2 border-b-2 border-indigo-400/20 hover:border-indigo-400/40", className)} {...props}>
           <NavBarList>  
-            <NavBarListItem>
+            <NavbarListItemLink href="/">
               <FontAwesomeIcon icon={ faHome } className="w-4 h4 mr-2" /> Home              
-            </NavBarListItem>        
+            </NavbarListItemLink>        
             
-            <NavBarListItem>
+            <NavbarListItemLink href="/games">
               <FontAwesomeIcon icon={ faGamepad } className="w-4 h4 mr-2" /> Games
-            </NavBarListItem> 
+            </NavbarListItemLink> 
 
-            <NavBarListItem>
+            <NavbarListItemLink href="/top-10">
               <FontAwesomeIcon icon={ faRankingStar } className="w-4 h4 mr-2" /> Top 10            
-            </NavBarListItem>  
+            </NavbarListItemLink>  
             
-            <NavBarListItem>
+            <NavbarListItemLink href="/walkthroughs">
               <FontAwesomeIcon icon={ faRoute } className="w-4 h4 mr-2" /> Walkthroughs            
-            </NavBarListItem>  
+            </NavbarListItemLink>  
           </NavBarList>          
         </nav>
       </div>
 
-      <button className="w-16 h-16 rounded-full bg-slate-900 fixed z-30 bottom-5 left-4 hover:bg-indigo-400/40 duration-300">
+      <Link href="/user" className="flex items-center justify-center w-16 h-16 rounded-full bg-slate-900 fixed z-30 bottom-5 left-3 hover:bg-indigo-400/40 duration-300">
         <FontAwesomeIcon icon={ faUser } className="w-9 h-9" />
-      </button>
+      </Link>
     </aside>
 
     // FIM SIDEBAR
