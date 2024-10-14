6c87049f8e5c73f8f89056db3d5f011ad5089cfb
juciley.costa <juciley.costa@e-unicred.com.br>
2024-10-14 17:15:47 -0300

    NextJS - Refactoring Navbar(reusability of components).

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,39 +1,55 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome" ;
 import { faUser, faHome, faGamepad, faRankingStar, faRoute } from "@fortawesome/free-solid-svg-icons";
 
+const NavBarList = ({ children}: {children: React.ReactNode}) => {
+  return (
+    <ul className="my-4 border-indigo-400/20 hover:border-indigo-400/40 w-full">
+      { children }
+    </ul>
+  );
+};
+
+const NavBarListItem = ({ children}: {children: React.ReactNode}) => {
+  return (
+    <li className="my-2 mx-2 p-3 rounded-lg bg-transparent hover:bg-indigo-400/40 cursor-pointer">
+      { children }
+    </li>
+  );
+};
+
 export const Navbar = () => {
   return(
     // SIDEBAR 
     
-    <aside className="mr-3 border-r-2 border-indigo-400/20 hover:border-indigo-400/60 hidden sm:flex w-full max-w-[200px] min-h-screen relative bg-slate-900 flex-col px-2 py-5">
+    <aside className="mr-4 border-r-2 border-indigo-400/20 hover:border-indigo-400/40 hidden sm:flex w-full max-w-[200px] min-h-screen relative bg-slate-900 flex-col px-2 py-5">
       <div className="fixed">
-        <div className="hover:scale-125 duration-300 w-40 h-40 z-30">
+        <div className="hover:scale-125 duration-300 w-32 h-32 z-30">
           <img src="/images/games.png" alt="Games Logo" />
         </div>
 
-        <nav className="flex flex-col mt-8 w-full gap-5">
-          <ul className="my-4 border-indigo-400/20 hover:border-indigo-400/60 w-full">
-            <li className="my-2 mx-2 p-3 rounded-lg bg-transparent hover:bg-slate-800 cursor-pointer">
-              <FontAwesomeIcon icon={ faHome } className="w-4 h4 mr-2" />
-              Home
-            </li>
-            <li className="my-2 mx-2 p-3 rounded-lg bg-transparent hover:bg-slate-800 cursor-pointer">
-              <FontAwesomeIcon icon={ faGamepad } className="w-4 h4 mr-2" />
-              Games
-            </li>
-            <li className="my-2 mx-2 p-3 rounded-lg bg-transparent hover:bg-slate-800 cursor-pointer">
-              <FontAwesomeIcon icon={ faRankingStar } className="w-4 h4 mr-2" />
-              Top 10
-            </li>
-            <li className="my-2 mx-2 p-3 rounded-lg bg-transparent hover:bg-slate-800 cursor-pointer">
-              <FontAwesomeIcon icon={ faRoute } className="w-4 h4 mr-2" />
-              Walkthroughs
-            </li>
-          </ul>
+        <nav className="flex flex-col mt-8 w-full gap-5 border-t-2 border-b-2 border-indigo-400/20 hover:border-indigo-400/40">
+          <NavBarList>  
+            <NavBarListItem>
+              <FontAwesomeIcon icon={ faHome } className="w-4 h4 mr-2" /> Home              
+            </NavBarListItem>        
+            
+            <NavBarListItem>
+              <FontAwesomeIcon icon={ faGamepad } className="w-4 h4 mr-2" /> Games
+            </NavBarListItem> 
+
+            <NavBarListItem>
+              <FontAwesomeIcon icon={ faRankingStar } className="w-4 h4 mr-2" /> Top 10            
+            </NavBarListItem>  
+            
+            <NavBarListItem>
+              <FontAwesomeIcon icon={ faRoute } className="w-4 h4 mr-2" /> Walkthroughs            
+            </NavBarListItem>  
+          </NavBarList>          
         </nav>
       </div>
-      <button className="w-11 h-11 rounded-full bg-slate-900 fixed z-30 bottom-5 left-4 hover:bg-slate-800 duration-300">
-        <FontAwesomeIcon icon={ faUser } size="2x" />
+
+      <button className="w-16 h-16 rounded-full bg-slate-900 fixed z-30 bottom-5 left-4 hover:bg-indigo-400/40 duration-300">
+        <FontAwesomeIcon icon={ faUser } className="w-9 h-9" />
       </button>
     </aside>
 
