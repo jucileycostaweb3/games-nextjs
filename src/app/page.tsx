4d6cc3f28844bcc163d5cb172528ce4ae751f2a1
juciley.costa <juciley.costa@e-unicred.com.br>
2024-10-15 16:48:41 -0300

    NextJS - Home content structure.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,77 @@
 export default function Home() {
-  return <h1>CURSO DE NEXTJS!</h1>
+  return (
+    <main className="flex flex-col w-full">
+      <div className="flex-center w-full h-[35vh] bg-orange-400">
+        <p>Algo Chamativo</p>
+      </div>
+      
+      <div className="container mx-auto my-4">
+        <div className="grid grid-cols-4 gap-4 h-[35vh]">
+          <div className="flex-center bg-indigo-500">A</div>
+          <div className="flex-center bg-purple-500">B</div>
+          <div className="flex-center bg-blue-500">C</div>
+          <div className="flex-center bg-emerald-500">D</div>
+        </div>
+      </div>
+
+      <div className="container mx-auto my-6">
+        <div className="grid grid-cols-12 gap-4">
+          <div className="col-span-8 flex flex-col gap-4">
+            
+            {/* ITEM 1*/}
+            <div className=" flex gap-4 bg-yellow-500 rounded-md">
+              <div>
+                <img src="/images/games.png" alt="Image description" className="w-auto h-52"/>
+              </div>
+              <div className="flex flex-col gap-2 py-4">
+                <h2 className="text-3xl">Algum título</h2>
+                <p className="flex-grow">Alguma descrição</p>
+                <button>Ler mais</button>
+              </div>
+            </div>
+
+            {/* ITEM 2*/}
+            <div className=" flex gap-4 bg-green-500 rounded-md">
+              <div>
+                <img src="/images/games.png" alt="Image description" className="w-auto h-52"/>
+              </div>
+              <div className="flex flex-col gap-2 py-4">
+                <h2 className="text-3xl">Algum título</h2>
+                <p className="flex-grow">Alguma descrição</p>
+                <button>Ler mais</button>
+              </div>
+            </div>
+
+            {/* ITEM 3*/}
+            <div className=" flex gap-4 bg-pink-500 rounded-md">
+              <div>
+                <img src="/images/games.png" alt="Image description" className="w-auto h-52"/>
+              </div>
+              <div className="flex flex-col gap-2 py-4">
+                <h2 className="text-3xl">Algum título</h2>
+                <p className="flex-grow">Alguma descrição</p>
+                <button>Ler mais</button>
+              </div>
+            </div>
+
+            {/* ITEM 4*/}
+            <div className=" flex gap-4 bg-rose-500 rounded-md">
+              <div>
+                <img src="/images/games.png" alt="Image description" className="w-auto h-52"/>
+              </div>
+              <div className="flex flex-col gap-2 py-4">
+                <h2 className="text-3xl">Algum título</h2>
+                <p className="flex-grow">Alguma descrição</p>
+                <button>Ler mais</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-4 bg-cyan-500">
+
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,16 +11,16 @@ export const Navbar = ({ className, ...props}: NavbarProps) => {
   return(
     // SIDEBAR 
     
-    <aside className="mr-4 border-r-2 border-indigo-400/20 hover:border-indigo-400/40 hidden sm:flex w-full max-w-[200px] min-h-screen relative bg-slate-900 flex-col px-2 py-5">
+    <aside className="border-r-2 hidden sm:flex flex-col w-full max-w-[200px] min-h-screen px-2 py-5 mr-0.5 bg-slate-900 border-indigo-400/20 hover:border-indigo-400/40">
       <div className="fixed">
-        <div className="z-30 hover:scale-125 duration-300">
-          <Image src="/images/games.png" width={128} height={128} alt="Games Logo"/>
+        <div className="flex flex-col items-center justify-center hover:scale-125 duration-300">
+          <Image src="/images/games.png" width={96} height={96} alt="Games Logo"/>
         </div>
 
         <nav className={cn("flex flex-col mt-8 w-full gap-5 border-t-2 border-b-2 border-indigo-400/20 hover:border-indigo-400/40", className)} {...props}>
           <NavBarList>  
             <NavbarListItemLink href="/">
-              <FontAwesomeIcon icon={ faHome } className="w-4 h4 mr-2" /> Home              
+              <FontAwesomeIcon icon={ faHome } className="w-4 h4 mr-2" /> Home
             </NavbarListItemLink>        
             
             <NavbarListItemLink href="/games">
@@ -28,19 +28,21 @@ export const Navbar = ({ className, ...props}: NavbarProps) => {
             </NavbarListItemLink> 
 
             <NavbarListItemLink href="/top-10">
-              <FontAwesomeIcon icon={ faRankingStar } className="w-4 h4 mr-2" /> Top 10            
+              <FontAwesomeIcon icon={ faRankingStar } className="w-4 h4 mr-2" /> Top 10
             </NavbarListItemLink>  
             
             <NavbarListItemLink href="/walkthroughs">
-              <FontAwesomeIcon icon={ faRoute } className="w-4 h4 mr-2" /> Walkthroughs            
+              <FontAwesomeIcon icon={ faRoute } className="w-4 h4 mr-2" /> Walkthroughs         
             </NavbarListItemLink>  
           </NavBarList>          
         </nav>
       </div>
 
-      <Link href="/user" className="flex items-center justify-center w-16 h-16 rounded-full bg-slate-900 fixed z-30 bottom-5 left-3 hover:bg-indigo-400/40 duration-300">
-        <FontAwesomeIcon icon={ faUser } className="w-9 h-9" />
-      </Link>
+      <div className="flex items-center left-8">
+        <Link href="/user" className="flex items-center justify-center w-16 h-16 rounded-full bg-slate-900 z-30 fixed top-[450px] left-11 hover:bg-indigo-400/40 duration-300">
+          <FontAwesomeIcon icon={ faUser } className="w-9 h-9" />
+        </Link>
+      </div>
     </aside>
 
     // FIM SIDEBAR
