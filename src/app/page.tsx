f516c31bb1dba8f7780b61878f039958fac3dc5d
juciley.costa <juciley.costa@e-unicred.com.br>
2024-10-16 10:33:53 -0300

    NextJS - Fetching articles from database.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import articles from "@/data/articles.json";
+import Article from "@/libs/database/Article";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+  const articles = await Article.get({ orderBy: { publishedAt: "desc" }, limit: 14 });
+  const highlightsArticles = articles.slice(0, 4);
+  const listArticles = articles.slice(4);
+
   return (
     <main className="flex flex-col w-full">
       <div className="flex-center w-full h-[35vh] bg-orange-400">
@@ -10,19 +15,19 @@ export default function Home() {
       
       <div className="container mx-auto my-4">
         <div className="grid grid-cols-4 gap-4 h-[35vh]">
-          {articles.splice(-4).map((article) => {
+          {highlightsArticles.map((highlight) => {
             return (
-              <div key={article.title} className="flex-center relative overflow-hidden">
+              <div key={highlight.title} className="flex-center relative overflow-hidden">
                 <div className="w-full h-full">
                   <Image className="w-full h-full object-cover transition duration-500 hover:scale-105 rounded-lg" 
-                    src={`/assets/images/articles/${article.image}`} 
+                    src={`/assets/images/articles/${highlight.image}`} 
                     width={1920} 
                     height={1080} 
-                    alt={article.title}
+                    alt={highlight.title}
                   />
                 </div>
                 <p className="absolute bottom-0 pt-6 pb-2 px-2 bg-gradient-to-t from-slate-900 via-slate-800 to-transparent w-full">
-                  {article.title}
+                  {highlight.title}
                 </p>
               </div>
             );
@@ -33,7 +38,7 @@ export default function Home() {
       <div className="container mx-auto my-6">
         <div className="grid grid-cols-12 gap-4">
           <div className="col-span-8 flex flex-col gap-4">
-            {articles.map((article) => {
+            {listArticles.map((article) => {
               return (
                 <div key={article.title} className="flex bg-slate-800 rounded-md py-4">
                   <div className="flex items-center">
@@ -47,7 +52,9 @@ export default function Home() {
                     </div>
                   </div>
                   <div className="w-full flex flex-col gap-2 pl-4">
-                    <h2 className="text-3xl mb-4 text-indigo-500">{article.title}</h2>
+                    <h2 className="text-3xl mb-4 text-indigo-500">
+                      {article.title}
+                    </h2>
                     <p className="flex-grow">{article.excerpt}</p>
                     <button className="bg-slate-700 hover:bg-indigo-400/40 rounded-lg px-4 py-2 inline max-w-max">Ler mais</button>
                   </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,7 +14,7 @@ export const Navbar = ({ className, ...props}: NavbarProps) => {
     <aside className="border-r-2 hidden sm:flex flex-col w-full max-w-[200px] min-h-screen px-2 py-5 mr-0.5 bg-slate-900 border-indigo-400/20 hover:border-indigo-400/40">
       <div className="fixed">
         <div className="flex flex-col items-center justify-center hover:scale-125 duration-300">
-          <Image src="/images/games.png" width={96} height={96} alt="Games Logo"/>
+          <Image src="/assets/images/games.png" width={96} height={96} alt="Games Logo"/>
         </div>
 
         <nav className={cn("flex flex-col mt-8 w-full gap-5 border-t-2 border-b-2 border-indigo-400/20 hover:border-indigo-400/40", className)} {...props}>
