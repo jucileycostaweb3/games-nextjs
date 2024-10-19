c4e0ec6a65ccd071d6cbbc8b3a43c2d803fe3d7d
juciley.costa <juciley.costa@e-unicred.com.br>
2024-10-19 17:48:37 -0300

    NextJS - Improving on getting random games.

--- src/services/Games.ts
+++ src/services/Games.ts
@@ -29,16 +29,13 @@ const GamesService = {
     const total = await Games.count({});
     const offset = Math.max(0, Math.floor(Math.random() * total) - limit);
 
-    //const ids = getRandomIntArrayInRange(0, total, limit);
-    //const where = { id: { in: ids } };
-    const data = await Games.get({ limit, offset });
-    const sorted = data.sort(() => {
-      return Math.random() > 0.5 ? 1 : -1;
-    });
+    const ids = getRandomIntArrayInRange(0, total, limit);
+    const where = { id: { in: ids } };
+    const data = await Games.get({ where, offset });
     const totalPages = Math.ceil(total / limit);
 
     return {
-      data: sorted,
+      data,
       metadata: {
         page: 1,
         limit,
